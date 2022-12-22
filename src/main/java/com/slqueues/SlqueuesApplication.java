425c5cc78f0e972c77bdb714a25a36a64c21fd34
neelima <neelumnbv@gmail.com>
2022-12-22 18:08:01 -0500

    spring boot gradle project

---
+++ src/main/java/com/slqueues/SlqueuesApplication.java
@@ -0,0 +1,13 @@
+package com.slqueues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlqueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlqueuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slqueues/SlqueuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slqueues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlqueuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
